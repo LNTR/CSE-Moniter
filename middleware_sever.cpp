dfe57cebec60e1995733e3c527b7e8fca989af1b
LNTR <101091694+LNTR@users.noreply.github.com>
2024-08-27 15:55:56 +0530

    Improvements to async connection

--- middleware_sever.cpp
+++ middleware_sever.cpp
@@ -14,6 +14,7 @@ using std::thread, std::cout, std::unordered_map;
 void handle_subscriber(ip::tcp::socket socket)
 {
     WebSubscriber subscriber(std::move(socket));
+    subscriber._test_api();
     for (;;)
     {
     };
@@ -36,17 +37,20 @@ ip::tcp::acceptor create_acceptor(string host, int port_number, asio::io_context
 int main()
 {
     std::system("cls");
-    // asio::io_context io_context;
+    asio::io_context io_context;
+    ip::tcp::acceptor subscriber_acceptor = create_acceptor(HOST, SUBSCRIBER_PORT_NUMBER, io_context);
+    ip::tcp::acceptor publisher_acceptor = create_acceptor(HOST, PUBLISHER_PORT_NUMBER, io_context);
 
-    // ip::tcp::acceptor subscriber_acceptor = create_acceptor(HOST, SUBSCRIBER_PORT_NUMBER, io_context);
-    // ip::tcp::acceptor publisher_acceptor = create_acceptor(HOST, PUBLISHER_PORT_NUMBER, io_context);
-
-    // while (true)
-    // {
-    //     publisher_acceptor.async_accept(io_context, [](const auto &error, ip::tcp::socket socket)
-    //                                     { thread(handle_publisher, std::move(socket)).detach(); });
-    //     subscriber_acceptor.async_accept(io_context, [](const auto &error, ip::tcp::socket socket)
-    //                                      { thread(handle_subscriber, std::move(socket)).detach(); });
-    //     io_context.run();
-    // }
+    while (true)
+    {
+        io_context.restart();
+        publisher_acceptor.async_accept(io_context, [&io_context](const auto &error, ip::tcp::socket socket)
+                                        {   thread(handle_publisher, std::move(socket)).detach();
+                                            io_context.stop(); });
+        subscriber_acceptor.async_accept(io_context, [&io_context](const auto &error, ip::tcp::socket socket)
+                                         {  thread(handle_subscriber, std::move(socket)).detach();
+                                            io_context.stop(); });
+        io_context.run();
+        cout << "Connected";
+    }
 }
